f52017c049d9b004990d93df1f48b7390701a7f6
Tabitha Miller <155591728+itsjustabitha@users.noreply.github.com>
2026-04-16 20:44:28 -0500

    first page

--- mood-tracker/src/main.jsx
+++ mood-tracker/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // Module 7 slide 52
+import 'bootstrap/dist/css/bootstrap.min.css';     // Bootstrap CSS loaded globally
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* BrowserRouter must wrap App - no other routing components work without it */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+);
